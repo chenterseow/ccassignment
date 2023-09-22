ba989cab1692eb388fc58a327840b769640a910e
Depression <88027109+chenterseow@users.noreply.github.com>
2023-09-22 11:30:13 +0800

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-customhost = "assignment-db.crx9f7gbpwdb.us-east-1.rds.amazonaws.com"
-customuser = "aws_user"
-custompass = "Bait3273"
-customdb = "assignment-db"
-custombucket = "jonas-employee"
-customregion = "us-east-1"
-
