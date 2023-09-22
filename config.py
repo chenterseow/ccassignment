78caecdb8f0af8aec5a8fb27f3e9e9b61cd9ce3d
Depression <88027109+chenterseow@users.noreply.github.com>
2023-09-22 11:57:35 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ customhost = "assignment-db.crx9f7gbpwdb.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "assignment-db"
-custombucket = "jonas-employee"
+custombucket = "ngjonas-bucket"
 customregion = "us-east-1"
 
