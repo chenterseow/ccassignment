e7503873ca521cfe534eec1ec0443ba1832c805d
Depression <88027109+chenterseow@users.noreply.github.com>
2023-09-23 11:54:54 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "try.crx9f7gbpwdb.us-east-1.rds.amazonaws.com"
-customuser = "jonas_try"
-custompass = "jonas_try"
-customdb = "try"
-custombucket = "try-employee"
+customhost = "assignment-db.crx9f7gbpwdb.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "assignment-db"
+custombucket = "ngjonas-bucket"
 customregion = "US East (N. Virginia) us-east-1"
 
