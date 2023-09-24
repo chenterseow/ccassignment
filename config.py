af2dfc32bdfc4eb34f20b64f972713974bab282f
Depression <88027109+chenterseow@users.noreply.github.com>
2023-09-24 13:39:57 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "assignment-db.crx9f7gbpwdb.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "assignment-db"
+custombucket = "ngjonas-bucket"
+customregion = "US East (N. Virginia) us-east-1"
+
